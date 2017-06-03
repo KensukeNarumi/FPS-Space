787503fcfb8ab99ccf3e7ac68871d3edca6e5ddd
DESKTOP-GICIUPD\kenna <kennarumi.wakawaa0201@gmail.com>
2017-06-03 22:29:02 +0900

    Create sniper

---
+++ Assets/Scripts/Snipe.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Snipe : MonoBehaviour {
+    bool SniperMode;
+    public GameObject snipe;
+    public GameObject Camera;
+	// Use this for initialization
+	void Start () {
+        SniperMode = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (SniperMode == false)
+            {
+                SniperMode = true;
+                snipe.SetActive(true);
+                Camera.GetComponent<Camera>().fieldOfView = 20;
+            }
+            else
+            {
+                SniperMode = false;
+                snipe.SetActive(false);
+                Camera.GetComponent<Camera>().fieldOfView = 60;
+            }
+        }
+	}
+}
